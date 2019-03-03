7be2ade2aa4b4d4c7f86865aadc75064a99c581e
DougASchu <dougaschu@gmail.com>
2019-03-03 13:07:15 -0600

    Finished Question 2

--- assignments/assignment3/join.py
+++ assignments/assignment3/join.py
@@ -10,14 +10,26 @@ mr = MapReduce.MapReduce()
 # =============================
 # Do not modify above this line
 
+# Douglas Schumacher, CS 639, March 2019
+
 # Implement the MAP function
-def mapper(# Provide the necessary inputs):
-    # YOUR CODE GOES HERE
+def mapper(record):
+    key = record[1]
+    mr.emit_intermediate(key, record)
 
 # Implement the REDUCE function
-def reducer(# Provide the necessary inputs):
-    # YOUR CODE GOES HERE
-
+def reducer(key, list_of_values):
+  # YOUR CODE GOES HERE
+  order_record = []
+  # Find the order record
+  for record in list_of_values:
+    if record[0] == 'order':
+      order_record = record
+  # Put together matches of order and line item records
+  for record in list_of_values:
+      if record != order_record:
+        mr.emit(order_record + record)
+  
 # Do not modify below this line
 # =============================
 if __name__ == '__main__':

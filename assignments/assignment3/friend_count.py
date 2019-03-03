65dabe0a2a21e0e3d4ab0fd60bc470103f7b1945
DougASchu <dougaschu@gmail.com>
2019-03-03 14:59:52 -0600

    Completed Question 3

--- assignments/assignment3/friend_count.py
+++ assignments/assignment3/friend_count.py
@@ -9,15 +9,19 @@ mr = MapReduce.MapReduce()
 
 # =============================
 # Do not modify above this line
+# Douglas Schumacher, CS 639, March 2019
 
 # Implement the MAP function
-def mapper(# Provide the necessary inputs):
+def mapper(record):
     # YOUR CODE GOES HERE
+  name = record[0]
+  mr.emit_intermediate(name, 1)
 
 # Implement the REDUCE function
-def reducer(# Provide the necessary inputs):
+def reducer(key, list_of_values):
     # YOUR CODE GOES HERE
-
+  friend_count = sum(list_of_values)
+  mr.emit((key, friend_count))
 # Do not modify below this line
 # =============================
 if __name__ == '__main__':

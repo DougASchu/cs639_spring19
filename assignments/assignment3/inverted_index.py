5fe806987f847bba8b33d3b634e687e58280ceb8
DougASchu <dougaschu@gmail.com>
2019-03-03 11:14:52 -0600

    Completed Problem 1

--- assignments/assignment3/inverted_index.py
+++ assignments/assignment3/inverted_index.py
@@ -10,13 +10,25 @@ mr = MapReduce.MapReduce()
 # =============================
 # Do not modify above this line
 
+# Douglas Schumacher, CS 639, March 2019
 # Implement the MAP function
-def mapper(# Provide the necessary inputs):
-    # YOUR CODE GOES HERE
+def mapper(record):
+    docID = record[0]
+    text = record[1]
+    word_list = text.split()
+    previous_words = []
+    for word in word_list:
+      if word not in previous_words:
+            mr.emit_intermediate(word, docID)
+            previous_words.append(word)
 
 # Implement the REDUCE function
-def reducer(# Provide the necessary inputs):
-    # YOUR CODE GOES HERE
+def reducer(key, list_of_values):
+    docID_list = []
+    for docID in list_of_values:
+      if docID not in docID_list:
+        docID_list.append(docID)
+    mr.emit((key, docID_list))
 
 # Do not modify below this line
 # =============================

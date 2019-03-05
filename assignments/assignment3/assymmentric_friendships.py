d3c2aeb199d1a252a664dd805d0942b87df6ae6e
DougASchu <dougaschu@gmail.com>
2019-03-05 17:56:00 -0600

    Finish Problem 4

--- assignments/assignment3/assymmentric_friendships.py
+++ assignments/assignment3/assymmentric_friendships.py
@@ -9,15 +9,27 @@ mr = MapReduce.MapReduce()
 
 # =============================
 # Do not modify above this line
+# Douglas Schumacher, CS 639, March 2019
 
 # Implement the MAP function
-def mapper(# Provide the necessary inputs):
-    # YOUR CODE GOES HERE
+def mapper(record):
+  # YOUR CODE GOES HERE
+  name1 = record[0]
+  name2 = record[1]
+  mr.emit_intermediate(min(name1, name2), [1, name1, name2])
 
 # Implement the REDUCE function
-def reducer(# Provide the necessary inputs):
-    # YOUR CODE GOES HERE
+def reducer(key, list_of_values):
+  # YOUR CODE GOES HERE
+  list_of_assym = list_of_values
+  for relationship in list_of_values:
+    for relation in list_of_values:
+      if relation == [1, relationship[2], relationship[1]]:
+        list_of_assym.remove(relationship)
+        list_of_assym.remove(relation)
 
+  for relation in list_of_assym:
+    mr.emit([relation[2], relation[1]])
 # Do not modify below this line
 # =============================
 if __name__ == '__main__':

1804efb9db7246c3a1a8ef87fefb33858ec61568
DougASchu <dougaschu@gmail.com>
2019-03-05 21:43:36 -0600

    Working on Problem 5

--- assignments/assignment3/multiply.py
+++ assignments/assignment3/multiply.py
@@ -10,13 +10,33 @@ mr = MapReduce.MapReduce()
 # =============================
 # Do not modify above this line
 
+# Douglas Schumacher, CS 639, March 2019
 # Implement the MAP function
-def mapper(# Provide the necessary inputs):
-    # YOUR CODE GOES HERE
+def mapper(record):
+  # YOUR CODE GOES HERE
+  matrix_name = record[0]
+  i = record[1]
+  j = record[2]
+  value = record[3]
+  if matrix_name == 'a':
+    for k in range(5):
+      mr.emit_intermediate((i, k), (m, j, value))
+  if matrix_name == 'b':
+    for k in range(5):
+      mr.emit_intermediate((j, k), (i, value)
+    
 
 # Implement the REDUCE function
-def reducer(# Provide the necessary inputs):
+def reducer(key, list_of_values):
     # YOUR CODE GOES HERE
+    total = 0
+    final_matrix_entry = []
+    for entry1 in list_of_values:
+      for entry2 in list_of_values:
+        if entry2[1] == entry1[1]:
+          total += entry2[1] * entry1[1]
+    final_matrix_entry = [key[0], key[1], total]
+    mr.emit(final_matrix_entry)
 
 # Do not modify below this line
 # =============================
